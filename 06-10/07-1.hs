b0c1d5a2d42adbffcac80b48226ed4171d8af7c2
Richard Li <richzli01@gmail.com>
2024-12-07 00:58:50 -0600

    07

---
+++ 06-10/07-1.hs
@@ -0,0 +1,77 @@
+import Control.Applicative
+import Control.Monad
+import Data.Bits
+import Data.Char
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as Set
+import System.IO
+
+--- string parsing ---
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p s = case break p s of
+    (a, []) -> [a]
+    (a, b) -> a : split p (drop 1 b)
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
+parseLines :: String -> [String]
+parseLines = dropWhileEnd null . split (=='\n')
+
+parseGroups :: String -> [[String]]
+parseGroups = split null . parseLines
+
+parseNumbers :: String -> [Int]
+parseNumbers s = read <$> filter (/="") (split isSpace s)
+
+--- utility ---
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+enumerate2d :: [[a]] -> [(Point, a)]
+enumerate2d grid = [((x, y), c)
+    | (x, row) <- enumerate grid
+    , (y, c) <- enumerate row
+    ]
+
+--- coordinates ---
+
+type Point = (Int, Int)
+
+d4, d8 :: [Point]
+d4 = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+d8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+
+zeroPoint :: Point
+zeroPoint = (0, 0)
+
+addPoint :: Point -> Point -> Point
+addPoint (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+subPoint :: Point -> Point -> Point
+subPoint (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
+
+mulPoint :: Int -> Point -> Point
+mulPoint c (x, y) = (c*x, c*y)
+
+neighbors :: [Point] -> Point -> [Point]
+neighbors d p = addPoint p <$> d
+
+bounds :: Int -> Int -> Point -> Bool
+bounds m n (x, y) = x >= 0 && x < m && y >= 0 && y < n
+
+---
+
+parseLine :: String -> (Int, [Int])
+parseLine s = (read left, parseNumbers right)
+    where left:right:_ = strip <$> split (==':') s
+
+main :: IO ()
+main = do
+    input <- parseLines <$> getContents
+    print $ sum $ fmap fst $ filter (\(target, l) -> target `elem` (foldl1' (\x y -> [(+), (*)] <*> x <*> y) (pure <$> l))) (parseLine <$> input)
