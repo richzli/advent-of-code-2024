5eb7184f2ea7996f7716b3e60881779ceeab7bd0
Richard Li <richzli01@gmail.com>
2024-12-08 02:00:46 -0600

    08

---
+++ 06-10/08-1.hs
@@ -0,0 +1,84 @@
+import Control.Applicative
+import Control.Monad
+import Data.Bits
+import Data.Char
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as Set
+import System.IO
+
+--- string parsing ---
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p s = case break p s of
+    (a, []) -> [a]
+    (a, b) -> a : split p (drop 1 b)
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
+parseLines :: String -> [String]
+parseLines = dropWhileEnd null . split (=='\n')
+
+parseGroups :: String -> [[String]]
+parseGroups = split null . parseLines
+
+parseNumbers :: String -> [Int]
+parseNumbers s = read <$> filter (/="") (split isSpace s)
+
+--- utility ---
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+enumerate2d :: [[a]] -> [(Point, a)]
+enumerate2d grid = [((x, y), c)
+    | (x, row) <- enumerate grid
+    , (y, c) <- enumerate row
+    ]
+
+--- coordinates ---
+
+type Point = (Int, Int)
+
+d4, d8 :: [Point]
+d4 = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+d8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+
+zeroPoint :: Point
+zeroPoint = (0, 0)
+
+addPoint :: Point -> Point -> Point
+addPoint (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+subPoint :: Point -> Point -> Point
+subPoint (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
+
+mulPoint :: Int -> Point -> Point
+mulPoint c (x, y) = (c*x, c*y)
+
+neighbors :: [Point] -> Point -> [Point]
+neighbors d p = addPoint p <$> d
+
+bounds :: Int -> Int -> Point -> Bool
+bounds m n (x, y) = x >= 0 && x < m && y >= 0 && y < n
+
+---
+
+pairs :: [a] -> [(a, a)]
+pairs l = concat $ zipWith (fmap . (,)) l (tail $ tails l)
+
+antinode :: (Point, Point) -> [Point]
+antinode (p1, p2) = [p1 `subPoint` d, p2 `addPoint` d]
+    where d = p2 `subPoint` p1
+
+main :: IO ()
+main = do
+    input <- parseLines <$> getContents
+    let m = length input
+    let n = length $ head input
+    let antennas = foldl' (\m (p, c) -> Map.insertWith (++) c [p] m) Map.empty $ filter ((/= '.') . snd) $ enumerate2d input
+    
+    print $ Set.size $ Set.filter (bounds m n) $ Map.foldr (\a b -> foldl' (flip Set.insert) b (concatMap antinode $ pairs a)) Set.empty antennas
