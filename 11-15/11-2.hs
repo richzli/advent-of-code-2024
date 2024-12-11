7203f161bfa47cb331ab4cd5f5ab8fc82cf62046
richzli <richzli01@gmail.com>
2024-12-10 23:23:04 -0600

    11

---
+++ 11-15/11-2.hs
@@ -0,0 +1,83 @@
+import Control.Applicative
+import Control.Monad
+import Data.Bits
+import Data.Char
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as Set
+import System.IO
+
+--- string parsing ---
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p s = case break p s of
+    (a, []) -> [a]
+    (a, b) -> a : split p (drop 1 b)
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
+parseLines :: String -> [String]
+parseLines = dropWhileEnd null . split (=='\n')
+
+parseGroups :: String -> [[String]]
+parseGroups = split null . parseLines
+
+parseNumbers :: String -> [Int]
+parseNumbers s = read <$> filter (/="") (split isSpace s)
+
+--- utility ---
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+enumerate2d :: [[a]] -> [(Point, a)]
+enumerate2d grid = [((x, y), c)
+    | (x, row) <- enumerate grid
+    , (y, c) <- enumerate row
+    ]
+
+--- coordinates ---
+
+type Point = (Int, Int)
+
+d4, d8 :: [Point]
+d4 = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+d8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+
+zeroPoint :: Point
+zeroPoint = (0, 0)
+
+addPoint :: Point -> Point -> Point
+addPoint (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+subPoint :: Point -> Point -> Point
+subPoint (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
+
+mulPoint :: Int -> Point -> Point
+mulPoint c (x, y) = (c*x, c*y)
+
+neighbors :: [Point] -> Point -> [Point]
+neighbors d p = addPoint p <$> d
+
+bounds :: Int -> Int -> Point -> Bool
+bounds m n (x, y) = x >= 0 && x < m && y >= 0 && y < n
+
+---
+
+halve :: [a] -> [[a]]
+halve l = (<*>) [fst, snd] . pure $ splitAt ((length l + 1) `div` 2) l
+
+blink :: Int -> [Int]
+blink n
+    | n == 0                         = [1]
+    | even (length (show n) `mod` 2) = read <$> halve (show n)
+    | otherwise                      = [n * 2024]
+
+main :: IO ()
+main = do
+    input <- parseNumbers . strip <$> getContents
+    let stones = foldr (\k m -> Map.insertWith (+) k 1 m) Map.empty input
+    print $ Map.foldr (+) 0 $ iterate (Map.foldrWithKey (\k a b -> foldr (\j m -> Map.insertWith (+) j a m) b $ blink k) Map.empty) stones !! 75
