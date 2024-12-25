f36ca39ecc084b6a136e0f9dbbdd029612d845b2
Richard Li <richzli01@gmail.com>
2024-12-24 23:10:43 -0600

    25

---
+++ 21-25/25-2.hs
@@ -0,0 +1,88 @@
+import Control.Applicative
+import Control.Monad
+import Data.Bifunctor
+import Data.Bits
+import Data.Char
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+import Data.Monoid
+import qualified Data.Set as Set
+import System.IO
+import Text.Read
+
+--- string parsing ---
+
+split :: (a -> Bool) -> [a] -> [[a]]
+split p s = case break p s of
+    (a, []) -> [a]
+    (a, b) -> a : split p (drop 1 b)
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
+parseLines :: String -> [String]
+parseLines = dropWhileEnd null . split (=='\n')
+
+parseGroups :: String -> [[String]]
+parseGroups = split null . parseLines
+
+parseNumbers :: String -> [Int]
+parseNumbers s = read <$> filter (/="") (split isSpace s)
+
+--- utility ---
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+enumerate2d :: [[a]] -> [(Point, a)]
+enumerate2d grid = [((x, y), c)
+    | (x, row) <- enumerate grid
+    , (y, c) <- enumerate row
+    ]
+
+--- coordinates ---
+
+type Point = (Int, Int)
+
+d4, d8 :: [Point]
+d4 = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+d8 = [(1, 1), (1, 0), (1, -1), (0, 1), (0, -1), (-1, 1), (-1, 0), (-1, -1)]
+
+zeroPoint :: Point
+zeroPoint = (0, 0)
+
+addPoint :: Point -> Point -> Point
+addPoint (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+subPoint :: Point -> Point -> Point
+subPoint (x1, y1) (x2, y2) = (x1 - x2, y1 - y2)
+
+mulPoint :: Int -> Point -> Point
+mulPoint c (x, y) = (c*x, c*y)
+
+neighbors :: [Point] -> Point -> [Point]
+neighbors d p = addPoint p <$> d
+
+bounds :: Int -> Int -> Point -> Bool
+bounds m n (x, y) = x >= 0 && x < m && y >= 0 && y < n
+
+---
+
+data Item = Lock [Int] | Key [Int]
+
+parseItem :: [String] -> Item
+parseItem ss = if all ((=='#') . head) ss' then Lock lens else Key lens
+    where
+        ss' = transpose ss
+        lens = length . filter (=='#') <$> ss'
+
+fit :: Item -> Item -> Bool
+fit i1 i2 = case (i1, i2) of
+    (Lock a, Key b) -> all (<=7) $ zipWith (+) a b
+    (Key a, Lock b) -> all (<=7) $ zipWith (+) a b
+    _ -> False
+
+main :: IO ()
+main = do
+    print $ "merry xmas :3"
